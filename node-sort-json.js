aa5f64d76baadb8904e081d24e231c518e403cc9
Kyle Mitchell <kyle@kemitchell.com>
2015-06-30 19:42:50 +0000

    Add brackets if needed

--- node-sort-json.js
+++ node-sort-json.js
@@ -3,10 +3,19 @@
 var input = ''
 
 process.stdin
+
+  // Buffer up stdin.
   .on('data', function(buffer) {
     input += buffer.toString()
   })
+
   .on('end', function() {
+    // Add brackets if needed
+    input = input.trim()
+    if (input[0] !== '[') {
+      input = '[' + input + ']'
+    }
+    // Parse, sort, stringify, and write.
     console.log(JSON.stringify(JSON.parse(input).sort()))
   })
 
